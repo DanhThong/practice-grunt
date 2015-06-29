920638c0d83409ede6dc743fad8b626ae4510fba
Danh Thong <danhthong@gmail.com>
2015-06-29 18:03:33 +0700

    Add menu footer

--- wp-content/plugins/dl-menu-category/dl-menu-category.php
+++ wp-content/plugins/dl-menu-category/dl-menu-category.php
@@ -10,21 +10,22 @@ License: GPL
 Copyright: Du lịch ẩm thực
 */
 
-function display_menu() {
-    $data = get_menu_by_category_id();
+function display_menu($atts) {
+    extract($atts);
+    $data = get_menu_by_category_id($cat);
     wp_enqueue_style( 'dl-menu-style', plugins_url('css/styles.css', __FILE__) );
     require ('template.php');
 }
 
 add_shortcode('menu_category', 'display_menu');
 
-function get_menu_by_category_id() {
+function get_menu_by_category_id($cat) {
     global $wpdb;
     $sql = "SELECT T.term_id, T.name, T.slug
         FROM  $wpdb->terms T
         LEFT JOIN $wpdb->term_taxonomy TT ON T.`term_id` = TT.`term_id`
         WHERE  `taxonomy` =  'category'
-        AND  `parent` = 6 ";
+        AND  `parent` = ".$cat;
     $results = $wpdb->get_results($sql);
     return $results;
 }
--- wp-content/plugins/dl-menu-category/template.php
+++ wp-content/plugins/dl-menu-category/template.php
@@ -1,9 +1,16 @@
-<div class="menu-category">
+<div class="menu-category clearfix">
     <ul class="menu">
+    <?php $i = 1;?>
     <?php foreach ($data as $item) { ?>
         <li class="menu-item">
             <a href="<?php echo get_category_link($item->term_id); ?>"><?php echo $item->name; ?></a>
         </li>
+    <?php
+    if ($i%7==0){
+        echo "</ul><ul class='menu'>";
+    }
+    $i++;
+    ?>
     <?php } ?>
     </ul>
 </div>
